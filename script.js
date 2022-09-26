e7bf1e30bc60bd5330fa04ce9f98bfdfde1ab569
aishu129 <aishu1292001@gmail.com>
2022-09-26 10:54:10 +0530

    Added status

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 window.addEventListener("load", () => {
   const form = document.querySelector("#new-task-form");
   const input = document.querySelector("#new-task-input");
+  const status_inp = document.querySelector("#status");
   const list_el = document.querySelector("#tasks");
+
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const task = input.value;
+    const status = status_inp.value;
+    
 
     if (!task) {
       alert("Please fill out task");
@@ -28,8 +32,23 @@ window.addEventListener("load", () => {
     task_input_el.style.width = "100%"
     task_input_el.setAttribute("readonly", "readonly")
 
+    const task_status_el = document.createElement("select")
+    const status_option1 = new Option("todo")
+    const status_option2 = new Option("pending")
+    const status_option3 = new Option("completed")
+    task_status_el.appendChild(status_option1)
+    task_status_el.appendChild(status_option2)
+    task_status_el.appendChild(status_option3)
+    // task_status_el.classList.add("text")
+    // task_status_el.type = "text"
+    task_status_el.value = status;
+    task_status_el.style.width = "30%"
+    task_status_el.style.marginRight = "20px"
+    task_status_el.setAttribute("readonly", "readonly")
+
 
     task_content_el.appendChild(task_input_el)
+    task_content_el.appendChild(task_status_el)
     
     const task_actions_el = document.createElement("div")
     task_actions_el.classList.add("actions")
